b14a88a86a2a8a38da41f00ce3cdca39b02b36fc
Björn Holm <bjh@kth.se>
2015-01-26 23:44:12 +0100

    upgraded Visual Studio 2013 and BWAPI 4.1.0 beta

--- AttackClosest/botAttackClosest.cpp
+++ AttackClosest/botAttackClosest.cpp
@@ -27,8 +27,7 @@ void BotAttackClosest::onStart()
 		Broodwar << "The following players are in this replay:" << std::endl;
 		
 		// Iterate all the players in the game using a std:: iterator
-		Playerset players = Broodwar->getPlayers();
-		for (auto& p : players)
+		for (auto& p : Broodwar->getPlayers())
 		{
 			// Only print the player if they are not an observer
 			if (!p->isObserver())
